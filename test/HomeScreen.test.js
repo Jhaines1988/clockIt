345efd3e5e11fe008ee6b83476c74ca8280aeddc
Jhaines1988 <josephthaines@gmail.com>
2022-07-28 18:47:59 -0600

    basic jest testing setup

---
+++ test/HomeScreen.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import HomeScreen from '../screens/Home Screen/HomeScreen';
+
+describe('<HomeScreen />', () => {
+  it('has 2 children', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+    expect(tree.children.length).toBe(2);
+  });
+});
