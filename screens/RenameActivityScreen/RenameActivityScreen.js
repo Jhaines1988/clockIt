07cfb7f5079059acc5aa6193d2f5921437fb2964
Jhaines1988 <josephthaines@gmail.com>
2022-08-17 13:26:50 -0600

    stubbed a rename screen after deciding this would be the best way to pass data around for updating

---
+++ screens/RenameActivityScreen/RenameActivityScreen.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { View, Text, Modal, StyleSheet } from 'react-native';
+import GradientView from '../UI/BackgroundContainer';
+
+function RenameActivityScreen({}) {
+  return (
+    <GradientView>
+      <View> </View>
+    </GradientView>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default RenameActivityScreen;
