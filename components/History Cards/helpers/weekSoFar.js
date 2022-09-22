a9c3736ab7bf8ea99056135b2eece6086e220c6d
Jhaines1988 <josephthaines@gmail.com>
2022-09-22 17:43:55 -0600

    Added helper function to extract users week data

---
+++ components/History Cards/helpers/weekSoFar.js
@@ -0,0 +1,10 @@
+const weekSoFar = function (item) {
+  const week = Object.keys(item).reduce((acc, key) => {
+    if (key !== 'id' && key !== 'name' && key !== 'totalTime') {
+      acc.push({ date: key, time: item[key] });
+    }
+    return acc;
+  }, []);
+  return week;
+};
+export default weekSoFar;
