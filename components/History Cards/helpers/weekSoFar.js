eac0f89bf524d7efd58566456316e1d0f6b4360d
Jhaines1988 <josephthaines@gmail.com>
2022-09-22 17:48:28 -0600

    Added helper dictionary object

--- components/History Cards/helpers/weekSoFar.js
+++ components/History Cards/helpers/weekSoFar.js
@@ -7,3 +7,13 @@ export const weekSoFar = function (item) {
   }, []);
   return week;
 };
+
+const dateOrderHelper = {
+  Sun: 0,
+  Mon: 1,
+  Tue: 2,
+  Wed: 3,
+  Thu: 4,
+  Fri: 5,
+  Sat: 6,
+};
