1b8e031101a3a87534edcfabe00a7527c756f12c
Jhaines1988 <josephthaines@gmail.com>
2022-09-22 13:59:49 -0600

    Edit styles

--- components/UI/ClockItLogoHeader.js
+++ components/UI/ClockItLogoHeader.js
@@ -5,37 +5,20 @@ const ClockItLogoHeader = () => {
   return (
     <View style={styles.logoContainer}>
       <Text style={styles.logoText}>Clock It</Text>
-      {/* <Image style={styles.logo} source={require('../../assets/clockitIcon.png')} /> */}
-      {/* <Text style={styles.logoSubText}>What did you do this week ?</Text> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   logoContainer: {
-    flex: 2,
     justifyContent: 'center',
-    alignItems: 'flex-start',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    // marginBottom: 70,
-    // backgroundColor: 'pink',
   },
   logoText: {
-    fontFamily: 'Manrope_800ExtraBold',
+    fontFamily: 'Manrope_600SemiBold',
     fontSize: 22,
-    lineHeight: 87,
     color: 'white',
     textAlign: 'center',
   },
-  logoSubText: {
-    fontFamily: 'Manrope_300Light',
-    fontSize: 16,
-    lineHeight: 22,
-    textAlign: 'center',
-    color: 'white',
-  },
-  logo: { width: 18, height: 18, alignSelf: 'flex-end', marginBottom: 12, marginLeft: 10 },
 });
 
 export default ClockItLogoHeader;
